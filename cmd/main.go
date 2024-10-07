08e3bdb4cd2eb1a3b72915a6f1a5153588e78274
Edu4rdoNeves <dudunekss@gmail.com>
2024-10-07 14:26:41 -0300

    Create a EasyStock

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/Edu4rdoNeves/EasyStrock/internal/api/server"
+	"github.com/Edu4rdoNeves/EasyStrock/internal/infrastructure/database"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load("../internal/api/config/.env")
+	if err != nil {
+		log.Fatal("Fail to load .env file")
+	}
+
+	database.Start()
+
+	server.Run()
 }
